5787e6a1fa4d917f0c1d66362e4bd67d4ff64015
Tyler Nielsen <tylernielsenn@gmail.com>
2025-08-26 13:55:44 -0700

    testing with streams

--- Testing/Redis/redis_test.py
+++ Testing/Redis/redis_test.py
@@ -1,6 +1,6 @@
 import redis
 import threading
-from time import sleep
+from time import sleep, time
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
@@ -16,23 +16,39 @@ r.hset('asdf', mapping={
   'one': 3
 })
 
-def producer_thread():
-  prodr = redis.Redis(host='localhost', port=6379, decode_responses=True)
-  for i in range(20):
-    prodr.set("val", i)
-    sleep(0.1)
+stream_key = "valley"
+r.xadd(stream_key, {'ts': time(), 'v': 0})
 
-def consumer_thread():
-  prodr = redis.Redis(host='localhost', port=6379, decode_responses=True)
-  for i in range(20):
-    print("Got: ", prodr.get("val"))
-    sleep(.5)
+v = int(r.xrevrange(name=stream_key, count=1)[0][1]['v'])
+print("Got:", v)
+v += 1
+r.xadd(stream_key, {'ts': time(), 'v': v})
 
-producer = threading.Thread(target=producer_thread)
-consumer = threading.Thread(target=consumer_thread)
+v = int(r.xrevrange(name=stream_key, count=1)[0][1]['v'])
+print("Got:", v)
 
-producer.start()
-consumer.start()
+# def producer_thread():
+#   prodr = redis.Redis(host='localhost', port=6379, decode_responses=True)
+#   for i in range(20):
+#     prodr.xadd(stream_key, {'ts': time(), 'v': i})
+#     sleep(1)
 
-producer.join()
-consumer.join()
+# def consumer_thread():
+#   prodr = redis.Redis(host='localhost', port=6379, decode_responses=True)
+#   for i in range(20):
+#     # print("Got: ", prodr.xread(
+#     #   count=1, 
+#     #   streams={stream_key:'$'},
+#     #   block=0
+#     # ))
+#     print("Got:", prodr.xrevrange(name=stream_key, count=1))
+#     sleep(.5)
+
+# producer = threading.Thread(target=producer_thread)
+# consumer = threading.Thread(target=consumer_thread)
+
+# producer.start()
+# consumer.start()
+
+# producer.join()
+# consumer.join()
